6d51a3099e3201aa37769e187bf8634b51e3127e
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-28 21:37:47 -0700

    Grabs some more elements from the DOM.

--- src/squares.js
+++ src/squares.js
@@ -1,5 +1,14 @@
 ;(function () {
-  var pink = document.getElementsByClassName('pink');
-  var yellow = document.getElementsByClassName('yellow');
-  var teal = document.getElementsByClassName('teal');
+  var pinkEl = document.querySelectorAll('.pink')[0];
+  var pinkXEl = pinkEl.querySelectorAll('.parent-square-x')[0];
+  var pinkYEl = pinkEl.querySelectorAll('.parent-square-y')[0];
+
+  var yellowEl = document.querySelectorAll('.yellow')[0];
+  var yellowXEl = yellowEl.querySelectorAll('.parent-square-x')[0];
+  var yellowYEl = yellowEl.querySelectorAll('.parent-square-y')[0];
+
+  var tealEl = document.querySelectorAll('.teal')[0];
+  var tealXEl = tealEl.querySelectorAll('.parent-square-x')[0];
+  var tealYEl = tealEl.querySelectorAll('.parent-square-y')[0];
+
 }(this));
