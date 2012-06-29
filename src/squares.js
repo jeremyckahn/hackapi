897867d19b729ffea3db05c75a959d39d794af10
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-28 21:28:28 -0700

    Grabs elements from the DOM.

---
+++ src/squares.js
@@ -0,0 +1,5 @@
+;(function () {
+  var pink = document.getElementsByClassName('pink');
+  var yellow = document.getElementsByClassName('yellow');
+  var teal = document.getElementsByClassName('teal');
+}(this));
